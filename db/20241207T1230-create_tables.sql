c7dfb3ac9378b7b922455d0314d34b4f55b3c45c
Karine Sarkisian <karine@uchicago.edu>
2024-12-07 03:24:30 -0600

    add db, schema for db, and flask

---
+++ db/20241207T1230-create_tables.sql
@@ -0,0 +1,41 @@
+create table if not exists users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    username VARCHAR(60) UNIQUE NOT NULL, 
+    password VARCHAR(60) NOT NULL,
+    api_key VARCHAR(40)
+); 
+
+create table if not exists channels (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name VARCHAR(100) UNIQUE NOT NULL
+); 
+
+create table if not exists messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    channel_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    reply_to INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(channel_id) REFERENCES channels(id),
+    FOREIGN KEY(reply_to) REFERENCES messages(id),
+);
+
+create table if not exists reactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    emoji text NOT NULL 
+    message_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL
+    FOREIGN KEY(message_id) REFERENCES messages(id), 
+    FOREIGN KEY(user_id) REFERENCES users(id) 
+); 
+
+create table if not exists user_message_read (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    message_id INTEGER NOT NULL, 
+    read_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(message_id) REFERENCES messages(id)
+); 
